234e8c249a61fdd227012947ffcc3eb13b450b78
Pablo Domínguez Blanco <pablo.dominguezblanco@gmail.com>
2017-10-17 16:58:07 +0200

    Add devtools folder

---
+++ devtools/devtools2/js/script.js
@@ -0,0 +1,17 @@
+(function toggleColor() {
+
+	var imagenes = document.getElementsByClassName("image");
+
+	for (var i=0; i<imagenes.length; i++) {
+		imagenes[i].addEventListener("click", function(e) {
+			e.target.classList.toggle("black-and-white");
+
+			var contador = 0;
+			while (contador < 1000) {
+				console.log("El número es " + contador);
+				contador++;
+			}
+		});
+	}
+
+})();
