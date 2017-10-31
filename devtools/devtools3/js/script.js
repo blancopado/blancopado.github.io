ae7087cac89aff81cb625f2e19c69531f8a4a1f3
Pablo Domínguez Blanco <pablo.dominguezblanco@gmail.com>
2017-10-31 11:28:40 +0100

    add devtools3 folder

---
+++ devtools/devtools3/js/script.js
@@ -0,0 +1,16 @@
+(function toggleColor() {
+
+	var imagenes = document.getElementsByClassName("image");
+
+	for (var i=0; i<imagenes.length; i++) {
+		imagenes[i].addEventListener("click", function(e) {
+			e.target.classList.toggle("black-and-white");
+
+			for (var i = 0; i < 100; i++) {
+				var newDiv = document.createElement("div");
+				document.body.appendChild(newDiv);
+			}
+		});
+	}
+
+})();
