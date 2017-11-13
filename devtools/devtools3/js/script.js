b87b8eb51a4cedbde59cbdde82638603e03d8d06
Pablo Domínguez Blanco <pablo.dominguezblanco@gmail.com>
2017-11-13 11:49:55 +0100

    Detach div elements

--- devtools/devtools3/js/script.js
+++ devtools/devtools3/js/script.js
@@ -6,10 +6,9 @@
 		imagenes[i].addEventListener("click", function(e) {
 			e.target.classList.toggle("black-and-white");
 
-			for (var i = 0; i < 100; i++) {
+			for (var i = 0; i < 500; i++) {
 				var newDiv = document.createElement("div");
 				newDiv.innerHTML = 'Hola';
-				document.body.appendChild(newDiv);
 			}
 
 		});
