4140d4c8307f1d3841b0b86bcd28a5ff6716bc49
Pablo Domínguez Blanco <pablo.dominguezblanco@gmail.com>
2017-11-13 18:20:17 +0100

    Detach div elements

--- devtools/devtools3/js/script.js
+++ devtools/devtools3/js/script.js
@@ -2,13 +2,17 @@
 
 	var imagenes = document.getElementsByClassName("image");
 
+	var nodoDiv = document.createElement('div');
+
 	for (var i=0; i<imagenes.length; i++) {
 		imagenes[i].addEventListener("click", function(e) {
 			e.target.classList.toggle("black-and-white");
-
-			for (var i = 0; i < 500; i++) {
-				var newDiv = document.createElement("div");
-				newDiv.innerHTML = 'Hola';
+			
+			for (var i = 0; i < 100; i++) {
+				var nodoP = document.createElement("p");
+				nodoP.innerHTML = 'Nuevo párrafo';
+				nodoDiv.appendChild(nodoP);
+				nodosSeparadosDelDom = nodoDiv;
 			}
 
 		});
